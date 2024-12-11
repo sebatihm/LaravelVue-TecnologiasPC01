2451c7ecef5a25890b83c2ca05b6dd5c4e989e83
Sebati HM <sebashermon003@gmail.com>
2024-12-10 22:41:08 -0600

    Seeders pt 1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -37,6 +37,95 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('12345678'),
                 'university_career'=> null
             ],
+            [
+                'id' => 3,
+                'role_id' => 1,
+                'tuition' => 'S22017007',
+                'name' => 'SEBASTIAN DE JESUS',
+                'last_name' => 'HERNANDEZ',
+                'mother_last_name' => 'MONTERO',
+                'email' => 'sebasmon020@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 4,
+                'role_id' => 1,
+                'tuition' => 'S22017041',
+                'name' => 'ROBERTO',
+                'last_name' => 'ZUÑIGA',
+                'mother_last_name' => 'ROMAN',
+                'email' => 'XxzhrineZxX@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 5,
+                'role_id' => 2,
+                'tuition' => 't22140331',
+                'name' => 'MAGDIEL',
+                'last_name' => 'MERCADO',
+                'mother_last_name' => 'CARRILO',
+                'email' => 'hectorivanalvarado@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 6,
+                'role_id' => 2,
+                'tuition' => 't22140332',
+                'name' => 'JAVIER',
+                'last_name' => 'PINO',
+                'mother_last_name' => 'HERRERA',
+                'email' => 'jpino@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 7,
+                'role_id' => 2,
+                'tuition' => 't22140333',
+                'name' => 'IRWING',
+                'last_name' => 'IBAÑEZ',
+                'mother_last_name' => 'CASTILLO',
+                'email' => 'iribañez@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 8,
+                'role_id' => 2,
+                'tuition' => 't22140334',
+                'name' => 'JOSE',
+                'last_name' => 'VERGARA',
+                'mother_last_name' => 'CAMACHO',
+                'email' => 'jvergara@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 9,
+                'role_id' => 2,
+                'tuition' => 't22140335',
+                'name' => 'LUIS',
+                'last_name' => 'MORALES',
+                'mother_last_name' => 'CARRILO',
+                'email' => 'luismorales04@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            [
+                'id' => 10,
+                'role_id' => 2,
+                'tuition' => 't22140336',
+                'name' => 'TILIN',
+                'last_name' => 'CIRO',
+                'mother_last_name' => 'INSANO',
+                'email' => 'autodidacta@gmail.com',
+                'password' => Hash::make('12345678'),
+                'university_career'=> null
+            ],
+            
         ]);
     }
 }
