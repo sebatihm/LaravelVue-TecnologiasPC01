128ad269df847300c03d9726c57739e83f4ed568
Sebati Hosan <163715872+sebatihm@users.noreply.github.com>
2024-12-12 11:03:45 -0600

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
